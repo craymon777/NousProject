4a5372911490e08fcab57ba781464687bf227341
craymon777 <secretday28@gmail.com>
2020-12-01 22:18:11 +0800

    Created first skill tree template

--- app/src/main/java/com/example/nous/Animal1Fragment.java
+++ app/src/main/java/com/example/nous/Animal1Fragment.java
@@ -2,11 +2,16 @@ package com.example.nous;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,9 @@ public class Animal1Fragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+
 
     public Animal1Fragment() {
         // Required empty public constructor
@@ -53,12 +61,27 @@ public class Animal1Fragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animal1, container, false);
+        View view = inflater.inflate(R.layout.fragment_animal1, container, false);
+
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //set onclickListener here
+
+
     }
 }
