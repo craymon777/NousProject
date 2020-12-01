d8e32552328538fb76760c6f8d49eab9c787d675
craymon777 <secretday28@gmail.com>
2020-12-01 16:40:40 +0800

    New Homepage Design

--- app/src/main/java/com/example/nous/MainActivity.java
+++ app/src/main/java/com/example/nous/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.nous;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vpTest;
+
+    private ArrayList<HomeCardModel> modelArrayList;
+    private HomeCardAdapter homeCardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vpTest = findViewById(R.id.vpTest);
     }
 }
